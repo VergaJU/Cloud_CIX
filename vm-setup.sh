2a0e9ab4bf5101dfd9157612e753c969004c3505
VergaJU <jacopoumberto.verga@gmail.com>
2022-05-19 15:13:07 +0100

    fix run anaconda installer

--- vm-setup.sh
+++ vm-setup.sh
@@ -96,6 +96,6 @@ sudo apt-get -y install libgl1-mesa-glx \
         libxml2-dev
 
 wget https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
-./Anaconda3-2021.05-Linux-x86_64.sh -b -p $HOME/anaconda
+bash ./Anaconda3-2021.05-Linux-x86_64.sh -b -p $HOME/anaconda
 #conda init
 #conda config --set auto_activate_base false
